7cd2605c3fcff123e71ed6aed7bd76474e8476db
Dongsoo Jung <100942621+DongsooJung@users.noreply.github.com>
2026-07-21 15:13:07 +0900

    푸터에서 입찰 준비센터를 첫 순서로 이동

--- 스타게이트 통합 네비.js
+++ 스타게이트 통합 네비.js
@@ -113,9 +113,9 @@
     </div>
     <div class="sg-footer__col">
       <h4>사업영역</h4>
+      <a href="${SITES.main.url}/procurement-center.html">입찰 준비센터</a>
       <a href="${SITES.main.url}">AI · 공간계량</a>
       <a href="${SITES.main.url}/monetization.html">수익화 전략</a>
-      <a href="${SITES.main.url}/procurement-center.html">입찰 준비센터</a>
       <a href="${SITES.shop.url}">디지털 상품</a>
       <a href="${SITES.edushop.url}">교육 스토어</a>
       <a href="${SITES.blog.url}">연구 블로그</a>
